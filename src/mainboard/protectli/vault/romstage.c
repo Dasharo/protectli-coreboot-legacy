d505bd7882e8473146f29866f175aeb26f0b8289
Michał Żygowski <michal.zygowski@3mdeb.com>
2019-02-19 11:28:33 +0100

    src/mainboard/protectli/vault/romstage.c: implement post raminit serial port reinitialization
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/protectli/vault/romstage.c
+++ src/mainboard/protectli/vault/romstage.c
@@ -19,9 +19,27 @@
 #include <console/console.h>
 #include <lib.h>
 #include <soc/gpio.h>
+#include <soc/lpc.h>
 #include <soc/pci_devs.h>
 #include <soc/romstage.h>
 #include <string.h>
+#include <superio/ite/common/ite.h>
+#include <superio/ite/it8613e/it8613e.h>
+
+#define SERIAL1_DEV PNP_DEV(0x2e, IT8613E_SP1)
+#define CLKIN_DEV PNP_DEV(0x2e, IT8613E_GPIO)
+
+void mainboard_configure_serial_after_raminit(void)
+{
+	/*
+	 * FSP Memory Init enables built-in serial legacy port.
+	 * Disable it and reconfigure serial port on SuperIO.
+	 */
+	pci_write_config32(PCI_DEV(0, LPC_DEV, 0), UART_CONT, (u32) 0);
+	ite_reg_write(CLKIN_DEV, 0x2c, 0x41); // disable K8 power seq
+	ite_reg_write(CLKIN_DEV, 0x2d, 0x02); // PCICLK 25MHz
+	ite_enable_serial(SERIAL1_DEV, CONFIG_TTYS0_BASE);
+}
 
 void mainboard_romstage_entry(struct romstage_params *rp)
 {

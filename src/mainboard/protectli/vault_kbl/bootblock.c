2f5326d8388045738d555d3ef55d17ecfbb9ce0f
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-12 16:51:59 +0100

    src/mainboard/protectli/vault_kbl/bootblock.c: Adding support for Yanling YL-KBR6L
    	Yanling YL-KBR6L has different SuperIO chip than FW6 Protectli so
    	adding KCONFIG statement with different board names help choose
    	correct SuperIO chip.
    
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/bootblock.c
+++ src/mainboard/protectli/vault_kbl/bootblock.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <bootblock_common.h>
+if (CONFIG(BOARD_PROTECTLI_FW6)) {
 #include <superio/ite/it8772f/it8772f.h>
 #include <superio/ite/common/ite.h>
 
@@ -13,4 +14,20 @@ void bootblock_mainboard_early_init(void)
 	ite_enable_3vsbsw(GPIO_DEV);
 	ite_kill_watchdog(GPIO_DEV);
 	ite_enable_serial(UART_DEV, CONFIG_TTYS0_BASE);
+}
+}
+
+if (CONFIG(BOARD_YANLING_YLKBR6L)) {
+#include <superio/ite/it8613e/it8613e.h>
+#include <superio/ite.common/ite.h>
+
+#define GPIO_DEV PNP_DEV(0x2e, IT8613E_GPIO)
+#define UART_DEV PNP_DEV(0x2e, IT8613E_SP1)
+
+void bootblock_mainboard_early_init(void)
+{
+	ite_reg_write(GPIO_DEV, 0x2c, 0x41); /* disable k8 power seq */
+	ite_reg_write(GPIO_DEV, 0x2d, 0x02); /*PCICLK 25MHz */
+	ite_enable_serial(UART_DEV, CONFIG_TTYS0_BASE);
+}
 }

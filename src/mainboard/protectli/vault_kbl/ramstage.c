3b4833dd8899cd0c643fc649c55610877e207691
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-12 17:05:39 +0100

    src/mainboard/protectli/vault_kbl/ramstage.c: Adding support for Yanling YL-KBR6L board
    	Yanling YL-KBR6L ramstage differs from FW6 Protectli by not
    setting PchThermalDeviceEnable parameter from FSP_SIL_UPD structure so
    Kconfig statement has been aded to choose between these two boards.
    
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/ramstage.c
+++ src/mainboard/protectli/vault_kbl/ramstage.c
@@ -10,10 +10,20 @@ void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 	 * Configure pads prior to SiliconInit() in case there's any
 	 * dependencies during hardware initialization.
 	 */
+	if (CONFIG(BOARD_PROTECTLI_FW6) {
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 
 	params->TurboMode = 1;
 	params->PchThermalDeviceEnable = 0;
 	params->PchPort61hEnable = 1;
 	params->CdClock = 3;
+	}
+
+	if (CONFIG(BOARD_YANLING_YLKBR6L)) {
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+
+	params->TurboMode = 1;
+	params->PchPort61hEnable = 1;
+	params->CdClock	= 3;
+	}
 }

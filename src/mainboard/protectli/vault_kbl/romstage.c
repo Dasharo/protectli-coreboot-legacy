32535257496f0378022934e49dbe1ddbfd5ea85c
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-19 14:22:19 +0100

    src/mainboard/protectli/vault_kbl/romstage.c: Removing DQ and DQS data
    	Since platform uses DDR4 DIMMs DQ and DQS parameters and maps are not needed.
    	Therefore function mainboard_fill_dq_map_data and
    	mainboard_fill_dqs_map_data are dropped.
    
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/romstage.c
+++ src/mainboard/protectli/vault_kbl/romstage.c
@@ -5,35 +5,6 @@
 #include <spd_bin.h>
 #include <string.h>
 
-static void mainboard_fill_dq_map_data(void *dq_map_ch0, void *dq_map_ch1)
-{
-	const u8 dq_map[2][12] = {
-	if(CONFIG(BOARD_PROTECTLI_FW6)) {
-		{ 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
-		  0x0F, 0x00, 0xFF, 0x00, 0xFF, 0x00 },
-		{ 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
-		  0x0F, 0x00, 0xFF, 0x00, 0xFF, 0x00 } };
-	}
-	if(CONFIG(BOARD_PROTECTLI_FW6D)) {
-		{ 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
-		  0x0F, 0x00, 0xFF, 0x00, 0xFF, 0x00 },
-		{ 0x33, 0xCC, 0x00, 0xCC, 0x33, 0xCC,
-		  0x33, 0x00, 0xFF, 0x00, 0xFF, 0x00 } };
-
-	}
-	memcpy(dq_map_ch0, dq_map[0], sizeof(dq_map[0]));
-	memcpy(dq_map_ch1, dq_map[1], sizeof(dq_map[1]));
-}
-
-static void mainboard_fill_dqs_map_data(void *dqs_map_ch0, void *dqs_map_ch1)
-{
-	const u8 dqs_map[2][8] = {
-		{ 0, 1, 2, 3, 4, 5, 6, 7 },
-		{ 1, 0, 2, 3, 4, 5, 6, 7 } };
-	memcpy(dqs_map_ch0, dqs_map[0], sizeof(dqs_map[0]));
-	memcpy(dqs_map_ch1, dqs_map[1], sizeof(dqs_map[1]));
-}
-
 static void mainboard_fill_rcomp_res_data(void *rcomp_ptr)
 {
 	const u16 RcompResistor[3] = { 121, 81, 100 };

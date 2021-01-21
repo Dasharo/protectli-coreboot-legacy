ad30608b17315e964ec78592cb457aa42a694550
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-21 09:18:52 +0100

    src/mainboard/protectli/vault_kbl/ramstage.c: Fixing argument of variant_mainboard_silicon_init_params function
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/ramstage.c
+++ src/mainboard/protectli/vault_kbl/ramstage.c
@@ -1,10 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <soc/gpio.h>
 #include <soc/ramstage.h>
 #include <baseboard/variants.h>
-#include <variant/gpio.h>
 
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 {
-	variant_mainboard_silicon_init_params(FSP_SIL_UPD *params);
+	variant_mainboard_silicon_init_params(params);
 }
+
+

f3c107f3f810cd4d143c446c5493296189698a41
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-25 12:31:16 +0100

    src/mainboard/protectli/vault_kbl/ramstage.c: Mainboard silicon init function unifying
    	Each variant of Protectli FW6 board has its own init code
    defined as body of appropiate IF statement.
    
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/ramstage.c
+++ src/mainboard/protectli/vault_kbl/ramstage.c
@@ -2,11 +2,22 @@
 
 #include <soc/gpio.h>
 #include <soc/ramstage.h>
-#include <baseboard/variants.h>
+#include <variant/gpio.h>
 
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 {
-	variant_mainboard_silicon_init_params(params);
+	if (CONFIG(BOARD_PROTECTLI_FW6)) {
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+	params->TurboMode = 1;
+	params->PchThermalDeviceEnable = 0;
+	params->PchPort61hEnable = 1;
+	params->CdClock = 3;
+	}
+
+	if (CONFIG(BOARD_PROTCTLI_FW6D)) {
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+	params->TurboMode = 1;
+	params->PchPort61hEnable = 1;
+	params->CdClock = 3;
+	}
 }
-
-

affff72797119bb739bd0be376f0545400f3d4f2
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-26 18:34:38 +0100

    src/mainboard/protectli/vault_kbl/ramstage.c: Restoring code commentary and fixing formattig.
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/ramstage.c
+++ src/mainboard/protectli/vault_kbl/ramstage.c
@@ -5,8 +5,13 @@
 #include <variant/gpio.h>
 
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
-{
+{	
+	/*
+	 * Configure pads prior SiliconInit() in case there's any
+	 * dependencies during hardware initialization.
+	 */
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+
 	params->TurboMode = 1;
 	params->PchThermalDeviceEnable = 0;
 	params->PchPort61hEnable = 1;

c2dc99bdb8d2da104bc7d54f509a0aefe47f4e17
Mateusz Kowalski <mateusz.kowalski@3mdeb.com>
2021-01-26 07:06:33 +0100

    src/mainboard/protectli/vault_kbl/ramstage.c: Simplyfing IF statements for various board variants
    	Instead having code for each variant within appropiate IF
    	statement, possibly duplicating it multiple times,
    	now only differences are added to those IF statements.
    
    Signed-off-by: Mateusz Kowalski <mateusz.kowalski@3mdeb.com>

--- src/mainboard/protectli/vault_kbl/ramstage.c
+++ src/mainboard/protectli/vault_kbl/ramstage.c
@@ -6,18 +6,11 @@
 
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 {
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+	params->TurboMode = 1;
 	if (CONFIG(BOARD_PROTECTLI_FW6)) {
-	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
-	params->TurboMode = 1;
 	params->PchThermalDeviceEnable = 0;
+	}
 	params->PchPort61hEnable = 1;
 	params->CdClock = 3;
-	}
-
-	if (CONFIG(BOARD_PROTCTLI_FW6D)) {
-	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
-	params->TurboMode = 1;
-	params->PchPort61hEnable = 1;
-	params->CdClock = 3;
-	}
 }
